e6b01a567741ae0c5ec2a384cee1270b702b9478
Takamurakeisuke <bp23114@shibaura-it.ac.jp>
2025-12-21 23:56:13 +0900

    ClienttoClientManagementMessageクラス作成

--- application/src/main/java/com/example/application/Client/ClienttoClientManagementMessage.java
+++ application/src/main/java/com/example/application/Client/ClienttoClientManagementMessage.java
@@ -1,5 +1,18 @@
 package com.example.application.Client;
 
 public class ClienttoClientManagementMessage {
+    String userName;
+    int passWord;
+    String taskName;
+    boolean authenticationFlag;
+    int gameRecord;
+
+    public ClienttoClientManagementMessage(String userName,int passWord,String taskName,boolean authenticationFlag,int gameRecord){
+this.userName=userName;
+this.passWord=passWord;
+this.taskName=taskName;
+this.authenticationFlag=authenticationFlag;
+this.gameRecord=gameRecord;
+    }
     
 }
